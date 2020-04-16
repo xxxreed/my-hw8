4f4ea7d99b17c0d7e828a028d8b7fc555fd22faa
xxxreed <wxie3430@usc.edu>
2020-04-15 22:05:46 -0700

    first comment

---
+++ src/build/precache-manifest.8bc17b74ff3f750f49156972584a2d96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16d95761c7d423532d49b84c75daa7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d221886c825e2215b230",
+    "url": "/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "15447f9d723e7c44b95c",
+    "url": "/static/css/main.53c2c431.chunk.css"
+  },
+  {
+    "revision": "d221886c825e2215b230",
+    "url": "/static/js/2.2add29be.chunk.js"
+  },
+  {
+    "revision": "5545cb2ced5a80c45120bcb0d570db6a",
+    "url": "/static/js/2.2add29be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15447f9d723e7c44b95c",
+    "url": "/static/js/main.a91f0700.chunk.js"
+  },
+  {
+    "revision": "28e77d4281d3f90b5194",
+    "url": "/static/js/runtime-main.3a36f248.js"
+  }
+]);
